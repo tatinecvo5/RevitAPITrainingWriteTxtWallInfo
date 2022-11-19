bec3eebd93bada087b6701ebcc66aa2762290890
dom31 <dom31@LAPTOP-19N5M1CV>
2022-11-19 11:55:14 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingWriteTxtWallInfo/Main.cs
@@ -0,0 +1,43 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingWriteTxtWallInfo
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            string wallInfo = string.Empty;
+
+            var walls = new FilteredElementCollector(doc)
+            .OfClass(typeof(Wall))
+                .Cast<Wall>()
+                .ToList();
+
+            foreach (Wall wall in walls)
+            {
+                double wallVolume = wall.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED).AsDouble();
+                wallInfo += $"{wall.Name}\t{wallVolume}{Environment.NewLine}";
+            }
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string txtPath = Path.Combine(desktopPath, "Список стен.txt");
+
+            File.WriteAllText(txtPath, wallInfo);
+
+            TaskDialog.Show("Сообщение", "Файл создан на рабочем столе");
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ RevitAPITrainingWriteTxtWallInfo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("RevitAPITrainingWriteTxtWallInfo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("RevitAPITrainingWriteTxtWallInfo")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a20de4b2-63f3-488a-ad48-050149d0f6df")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
